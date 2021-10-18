9f7be5ad69480d34c2685559d337c3a7eb09d101
Humberto Solano <hsolano_ext@amco.mx>
2021-10-18 09:46:21 -0500

    First commit

---
+++ app/src/main/java/mx/com/na_at/hsolano/na_atpeople/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.com.na_at.hsolano.na_atpeople
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
