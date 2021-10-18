87e85d8f9be045d92ff7776a7370cb071166bbf2
Humberto Solano <hsolano_ext@amco.mx>
2021-10-18 13:01:39 -0500

    Add toolbar view and bottom navigation layout

---
+++ app/src/main/java/mx/com/na_at/hsolano/na_atpeople/view/activity/HomeActivity.kt
@@ -0,0 +1,26 @@
+package mx.com.na_at.hsolano.na_atpeople.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import mx.com.na_at.hsolano.na_atpeople.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+
+        val layoutExpandedToolbar = findViewById<ConstraintLayout>(R.id.layout_expanded_toolbar)
+        val layoutCollapsedToolbar = findViewById<ConstraintLayout>(R.id.layout_collapsed_toolbar)
+
+        val navigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navigationController = findNavController(R.id.fragment_navigation_host)
+
+        navigationView.setupWithNavController(navigationController)
+    }
+}
